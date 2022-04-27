20a781f02c50d761d066630eb1f029af732d0993
Alexandre Zelante <alexandre.zelante@outlook.com>
2022-04-26 23:05:27 -0300

    merge tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -21,14 +21,6 @@ describe("LocalSavePurchases", () => {
   test("Should not delete or insert cache on sut.init", () => {
     const { cacheStore } = makeSut();
     expect(cacheStore.messages).toEqual([]);
-
-  });
-
-  test("Should delete old cache on sut.save", async () => {
-    const { cacheStore, sut } = makeSut();
-    await sut.save(mockPurchases());
-    expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
-    expect(cacheStore.deleteKey).toBe('purchases');
   });
 
   test("Should not insert new cache if delete fails", async () => {
@@ -44,6 +36,8 @@ describe("LocalSavePurchases", () => {
     const purchases = mockPurchases()
     await sut.save(purchases);
     expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
+    expect(cacheStore.deleteKey).toBe('purchases');
+    expect(cacheStore.insertKey).toBe('purchases');
     expect(cacheStore.insertValues).toEqual(purchases);
   });
 
