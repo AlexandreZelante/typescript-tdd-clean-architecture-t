ea1e5c5b4086ac363e237cee9867b46db4f1f16f
Alexandre Zelante <alexandre.zelante@outlook.com>
2022-04-26 23:16:27 -0300

    ensure LocalSavePurchases resolves successfully

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -36,7 +36,7 @@ describe("LocalSavePurchases", () => {
 
     const { cacheStore, sut } = makeSut(timestamp);
     const purchases = mockPurchases()
-    await sut.save(purchases);
+    const promise = sut.save(purchases);
     expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
     expect(cacheStore.deleteKey).toBe('purchases');
     expect(cacheStore.insertKey).toBe('purchases');
@@ -44,6 +44,8 @@ describe("LocalSavePurchases", () => {
       timestamp,
       value: purchases
     });
+    // toBeFalsy because promise returns undefined 
+    await expect(promise).resolves.toBeFalsy()
   });
 
   test("Should throw if insert throws", async () => {
