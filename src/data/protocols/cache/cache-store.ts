2aee9954f9b4c97ebf3ffcf870f83af0c777116a
Alexandre Zelante <alexandre.zelante@outlook.com>
2022-04-26 23:23:42 -0300

    Add replace function

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -3,4 +3,5 @@ export class CacheStore {
   delete: (key: string) => void;
   // value should have "any" type because is a generic component
   insert: (key: string, value: any) => void;
+  replace: (key: string, value: any) => void;
 }
